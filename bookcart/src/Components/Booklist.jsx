90f31cbea78d5df3e6dac7143babd4a5e2c20539
Akash Nuka <akashnuka04@gmail.com>
2024-11-05 15:36:54 +0530

    vercel

--- bookcart/src/Components/Booklist.jsx
+++ bookcart/src/Components/Booklist.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Booklist(){
-    return(
+function Booklist() {
+    return (
         <>
-            <h1>Booklist for bookcart</h1>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div>Book 1</div>
+                <div>Book 2</div>
+                <div>Book 3</div>
+            </div>
         </>
-    )
+    );
 }
+
 export default Booklist;
--- bookcart/src/Components/Navbar.jsx
+++ bookcart/src/Components/Navbar.jsx
@@ -3,23 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
     return (
-        <div className="navbar">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/">Bookcart</a>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/about">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/contact">Contact</a>
-                        </li>
-                    </ul>
+        <div className="navbar" style={{ width: '100%' }}>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ width: '100%' }}>
+                <div className="container-fluid">
+                    <div className="row w-100">
+                        <div className="col-md-4">
+                            <a className="navbar-brand" href="/">Bookcart</a>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="collapse navbar-collapse">
+                                <ul className="navbar-nav mr-auto">
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="/">Home</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="/about">About</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="/contact">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <form className="form-inline my-2 my-lg-0 d-flex">
+                                <input className="form-control mr-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <button style={{ backgroundColor: 'blue', borderRadius: '12px', color: 'white' }}>fverdsv</button>
             </nav>
         </div>
     );
