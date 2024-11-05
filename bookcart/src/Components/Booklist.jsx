0339cd8f6f84984f307a905aea2fe96bcf3ae6de
Akash Nuka <akashnuka04@gmail.com>
2024-11-05 16:01:14 +0530

    vite

--- bookcart/src/Components/Booklist.jsx
+++ bookcart/src/Components/Booklist.jsx
@@ -1,15 +1,52 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import hp2 from '../assets/images/hp2.jpg';
+import hp1 from '../assets/images/hp1.jpg';
+import hp3 from '../assets/images/hp3.jpg';
 
-function Booklist() {
+const books = [
+    {
+        id: 1,
+        title: "harrry potter and the philosopher's stone",
+        author: "jk rowling",
+        image: hp2,
+        description: "harry potter and the philosopher's stone"
+    },
+    {
+        id: 2,
+        title: "harry potter and the chamber of secrets",
+        author: "jk rowling",
+        image: hp1,
+        description: "harry potter and the chamber of secrets"
+    },
+    {
+        id: 3,
+        title: "harry potter and the prisoner of azkaban",
+        author: "jk rowling",
+        image:  hp3,
+        description: "harry potter and the prisoner of azkaban"
+    }
+];
+
+function BookList() {
     return (
-        <>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <div>Book 1</div>
-                <div>Book 2</div>
-                <div>Book 3</div>
+        <div className="container">
+            <div className="row">
+                {books.map(book => (
+                    <div className="col-md-4" key={book.id}>
+                        <div className="card mb-4">
+                            <img src={book.image} className="card-img-top" alt={book.title} />
+                            <div className="card-body">
+                                <h5 className="card-title">{book.title}</h5>
+                                <p className="card-text">by {book.author}</p>
+                                <p className="card-text">{book.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-        </>
+        </div>
     );
 }
 
-export default Booklist;
+export default BookList;
--- bookcart/src/Components/Navbar.jsx
+++ bookcart/src/Components/Navbar.jsx
@@ -3,14 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
     return (
-        <div className="navbar" style={{ width: '100%' }}>
+        <div className="navbar" style={{ width: '100%', border: '2px solid #007bff' }}>
             <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ width: '100%' }}>
                 <div className="container-fluid">
                     <div className="row w-100">
-                        <div className="col-md-4">
+                        <div className="col-md-8 d-flex justify-content-between">
                             <a className="navbar-brand" href="/">Bookcart</a>
-                        </div>
-                        <div className="col-md-4">
                             <div className="collapse navbar-collapse">
                                 <ul className="navbar-nav mr-auto">
                                     <li className="nav-item">
@@ -25,7 +23,7 @@ function Navbar() {
                                 </ul>
                             </div>
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-4 d-flex justify-content-end">
                             <form className="form-inline my-2 my-lg-0 d-flex">
                                 <input className="form-control mr-2" type="search" placeholder="Search" aria-label="Search" />
                                 <button className="btn btn-outline-success" type="submit">Search</button>
